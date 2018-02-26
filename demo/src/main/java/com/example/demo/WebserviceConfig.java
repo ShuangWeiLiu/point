ae88c2fcb8f8c1a891c29d88dbc34222b6c2a999
刘双威 <1023225325@qq.com>
2018-02-26 18:17:04 +0800

    webservice

---
+++ demo/src/main/java/com/example/demo/WebserviceConfig.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+
+@Configuration
+public class WebserviceConfig extends WsConfigurerAdapter {
+
+	
+	
+}
